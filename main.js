9b7c2ee7cca057dd13081ac4eea2cac23b2ed265
Umar <umar97@hotmail.co.uk>
2021-06-05 01:45:08 +0100

    all done except css

--- main.js
+++ main.js
@@ -1,14 +1,56 @@
-let foodToSearch = null;
+const YOUR_APP_ID = "993b6c5e";
+const YOUR_APP_KEY = "77c226a9415cc2708301b97c64d04290";
+const requestUrl = `https://api.edamam.com/search?q=kale&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
+
+let food = null;
+
+let endPoint = document.getElementById("recipes");
 
 function handleRecipeClick() {
-  fetchRecipe(foodToSearch);
+  fetchRecipe(food);
 }
 
 function handleFoodChange() {
-  foodToSearch = document.querySelector("#food-input").value;
+  food = document.querySelector("#food-input").value;
 }
 
 async function fetchRecipe(food) {
-  //--- write your code below ---
-  //--- write your code above ---
+  let response = await fetch(
+    `https://api.edamam.com/search?q=${food}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`
+  );
+  let data = await response.json();
+
+  endPoint.innerHTML = "";
+  getRecipesFromAPI(data);
+  console.log(data);
+}
+
+function getRecipesFromAPI(findings) {
+  for (let i = 0; i < 5; i++) {
+    let imgUrl = findings.hits[i].recipe.image;
+    let img = document.createElement("img");
+    img.setAttribute("src", imgUrl);
+
+    let labelName = findings.hits[i].recipe.label;
+    let label = document.createElement("a");
+    let labelhref = findings.hits[i].recipe.url;
+    label.setAttribute("href", labelhref);
+    label.innerText = labelName;
+
+    let caloriesNumber = findings.hits[i].recipe.calories;
+    let calories = document.createElement("p");
+    calories.innerText = `${Math.round(caloriesNumber)} calories`;
+
+    // let cuisineOrigin = findings.hits[i].recipe.cuisineType[0];
+    // let cuisines = document.createElement("p");
+    // cuisines.innerText = `${cuisineOrigin} cuisine`;
+    //didn't work, not sure why
+
+    let divForTheRecipe = document.createElement("div");
+
+    divForTheRecipe.append(img, label, calories);
+
+    endPoint.appendChild(divForTheRecipe);
+    divForTheRecipe.setAttribute("class", "plate");
+  }
 }
